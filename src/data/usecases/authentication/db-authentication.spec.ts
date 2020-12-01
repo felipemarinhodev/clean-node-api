41c502f6b4f08db6089ae4ccbfa15f152165d1f2
Felipe Marinho <felipmarinho@gmail.com>
2020-11-30 21:08:29 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,12 @@
-import { AccountModel } from '../../../domain/model/account'
-import { AuthenticationModel } from './../../../domain/usecases/authentication'
-import { TokenGenerator } from './../../protocols/criptography/token-generator'
-import { HashComparer } from './../../protocols/criptography/hash-comparer'
-import { UpdateAccessTokenRepository } from './../../protocols/db/update-access-token-repository'
-import { LoadAccountByEmailRepository } from './../../protocols/db/load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
+import {
+  AccountModel,
+  HashComparer,
+  TokenGenerator,
+  AuthenticationModel,
+  UpdateAccessTokenRepository,
+  LoadAccountByEmailRepository
+} from './db-authentication-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,9 +1,11 @@
-import { TokenGenerator } from './../../protocols/criptography/token-generator'
-import { HashComparer } from './../../protocols/criptography/hash-comparer'
-import { Authentication, AuthenticationModel } from './../../../domain/usecases/authentication'
-import { UpdateAccessTokenRepository } from './../../protocols/db/update-access-token-repository'
-import { LoadAccountByEmailRepository } from './../../protocols/db/load-account-by-email-repository'
-
+import {
+  HashComparer,
+  TokenGenerator,
+  Authentication,
+  AuthenticationModel,
+  UpdateAccessTokenRepository,
+  LoadAccountByEmailRepository
+} from './db-authentication-protocols'
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
