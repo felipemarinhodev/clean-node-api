a5bea84632e2a99610c9ec0e37188cd6826c6909
Felipe Marinho <felipmarinho@gmail.com>
2021-02-01 05:29:44 -0300

    refactor: adjustment at the imports

--- src/Infra/db/mongodb/account/account-mongo-repository.ts
+++ src/Infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,8 +1,8 @@
 import { AccountModel } from '../../../../domain/model/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
-import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
-import { UpdateAccessTokenRepository } from './../../../../data/protocols/db/update-access-token-repository'
-import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
+import { UpdateAccessTokenRepository } from './../../../../data/protocols/db/account/update-access-token-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
